ec729a4902d2a0ed3896198250a5e2f72d8af555
PatelDhruti <dhrutip2005@gmail.com>
2024-12-26 14:37:18 +0530

    Deployment Changes

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const dotenv = require("dotenv");
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://inventory-management-puce-rho.vercel.app',
   credentials: true
 }));
 dotenv.config();
